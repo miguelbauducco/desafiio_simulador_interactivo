7cd9076ac3aaa9183848b7a2886daa75c6a61f2b
miguelbauducco <miguel.bau14@gmail.com>
2022-06-28 21:30:01 -0300

    finalizado

--- script.js
+++ script.js
@@ -1,29 +1,58 @@
+class Producto{
+    constructor(valor,articulo){
+        this.valor=valor;
+        this.articulo=articulo;
+    }
+
+    iva(){
+        this.valor=this.valor*1.21;
+    }
+
+
+}
+
+const productos = [new Producto( 50000, "tv"),
+                new Producto( 80000, "pc"),
+                new Producto( 40000, "celular"),
+                new Producto(20000, "tablet")];
+
+
+for (const producto of productos){
+    producto.iva();
+}
+
+
+
+console.log(productos[parseInt(prompt("Ingrese 0 si desea TV, 1 para PC, 2 CELULAR y 3 TABLET"))]);
+
+
+
 let cuotas = parseInt(prompt("Ingrese cantidad de cuotas: "));
-let precio = parseInt(prompt("Ingrese precio del producto: "));
 let precioFinal;
+
 
 
 function calculadoraDeCuotas(cantidadDeCuotas,monto,total){
 
-        switch(cantidadDeCuotas){
+        switch(cantidadDeCuotas){         
             case 3: 
-            total=parseInt(monto/3);
+            total=Math.round(monto/cantidadDeCuotas);
             break;
 
             case 6: 
-            total=parseInt(monto/6);
+            total=Math.round(monto/cantidadDeCuotas);
             break;
 
             case 12: 
-            total=parseInt(monto/12);
+            total=Math.round(monto/cantidadDeCuotas);
             break;
 
             case 18: 
-            total=parseInt(monto/18);
+            total=Math.round(monto/cantidadDeCuotas);
             break;
 
             case 24: 
-            total=parseInt(monto/24);
+            total=Math.round(monto/cantidadDeCuotas);
             break;
 
             default:
@@ -32,8 +61,12 @@ function calculadoraDeCuotas(cantidadDeCuotas,monto,total){
 
 return total;
 }
-console.log("Debe abonar "+cuotas+" de ");
-console.log(calculadoraDeCuotas(cuotas,precio,precioFinal));
+
+
+console.log("Debe abonar "+cuotas+" cuotas de ");
+console.log(calculadoraDeCuotas(cuotas, productos.valor,precioFinal)+" pesos.");
+
+
 
 
 
